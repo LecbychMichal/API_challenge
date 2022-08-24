89e03d024e588649b626385c1355c05c6ce4aa56
MICHAL <lecbych.michal@gmail.com>
2022-08-24 15:40:45 +0200

    query-filter countryCode added

--- backend/country_API/views.py
+++ backend/country_API/views.py
@@ -2,15 +2,18 @@ from .models import Countries
 from .serializers import CountryListSerializer, CountryDetailSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from .paginations import CustomPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CountryListView(ListCreateAPIView):
     """
-    Post and get method for listing all the countries.
+    Post and get method for listing all the countries. Query by countryCode
     """
     queryset = Countries.objects.all()
     serializer_class = CountryListSerializer
     pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['countryCode']
 
 class CountryDetailView(RetrieveUpdateAPIView):
     """
